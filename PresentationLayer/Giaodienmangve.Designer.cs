7e025625a91c6c07b9a2b117780430c8eede763a
justhuuthang <95196568+justhuuthang@users.noreply.github.com>
2025-04-18 15:30:23 +0700

    gia dien
    gaio dien

---
+++ PresentationLayer/Giaodienmangve.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace PresentationLayer
+{
+    partial class Giaodienmangve
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            btnBack = new Button();
+            label1 = new Label();
+            panel2 = new Panel();
+            txbSearch = new TextBox();
+            panel3 = new Panel();
+            panel7 = new Panel();
+            panel6 = new Panel();
+            listView1 = new ListView();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            label2 = new Label();
+            button1 = new Button();
+            panel8 = new Panel();
+            numericUpDown1 = new NumericUpDown();
+            button2 = new Button();
+            btn_Thanhtoan = new Button();
+            lv_Menu = new ListView();
+            imageList1 = new ImageList(components);
+            btn_search = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            panel8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnBack);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(4, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(960, 59);
+            panel1.TabIndex = 0;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(8, 15);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(94, 29);
+            btnBack.TabIndex = 1;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(403, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Bán mang về";
+            label1.Click += label1_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(btn_search);
+            panel2.Controls.Add(txbSearch);
+            panel2.Location = new Point(6, 67);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(178, 469);
+            panel2.TabIndex = 1;
+            // 
+            // txbSearch
+            // 
+            txbSearch.Location = new Point(34, 3);
+            txbSearch.Name = "txbSearch";
+            txbSearch.Size = new Size(141, 27);
+            txbSearch.TabIndex = 0;
+            txbSearch.Text = "Tìm kiếm";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(panel7);
+            panel3.Controls.Add(panel6);
+            panel3.Controls.Add(listView1);
+            panel3.Location = new Point(744, 118);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(221, 339);
+            panel3.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            panel7.Location = new Point(3, 345);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(208, 41);
+            panel7.TabIndex = 5;
+            // 
+            // panel6
+            // 
+            panel6.Location = new Point(0, 345);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(211, 41);
+            panel6.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(3, 3);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(215, 333);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(lv_Menu);
+            panel4.Location = new Point(190, 67);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(548, 469);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(label2);
+            panel5.Controls.Add(button1);
+            panel5.Location = new Point(744, 68);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(220, 47);
+            panel5.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(68, 13);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Giỏ hàng";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(167, 9);
+            button1.Name = "button1";
+            button1.Size = new Size(44, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Xóa";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel8
+            // 
+            panel8.Controls.Add(numericUpDown1);
+            panel8.Controls.Add(button2);
+            panel8.Controls.Add(btn_Thanhtoan);
+            panel8.Location = new Point(744, 463);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(221, 73);
+            panel8.TabIndex = 5;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(10, 40);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(108, 27);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(6, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 35);
+            button2.TabIndex = 1;
+            button2.Text = "Giảm giá";
+            button2.TextAlign = ContentAlignment.TopCenter;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Thanhtoan
+            // 
+            btn_Thanhtoan.Location = new Point(119, 3);
+            btn_Thanhtoan.Name = "btn_Thanhtoan";
+            btn_Thanhtoan.Size = new Size(99, 61);
+            btn_Thanhtoan.TabIndex = 0;
+            btn_Thanhtoan.Text = "Thanh toán";
+            btn_Thanhtoan.UseVisualStyleBackColor = true;
+            // 
+            // lv_Menu
+            // 
+            lv_Menu.Location = new Point(6, 6);
+            lv_Menu.Name = "lv_Menu";
+            lv_Menu.Size = new Size(539, 457);
+            lv_Menu.TabIndex = 0;
+            lv_Menu.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // btn_search
+            // 
+            btn_search.Image = Properties.Resources.ic_search;
+            btn_search.Location = new Point(1, 2);
+            btn_search.Name = "btn_search";
+            btn_search.Size = new Size(34, 31);
+            btn_search.TabIndex = 1;
+            btn_search.UseVisualStyleBackColor = true;
+            // 
+            // Giaodienmangve
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(977, 542);
+            Controls.Add(panel8);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Giaodienmangve";
+            Text = "Bán mang về";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel8.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Button btnBack;
+        private Panel panel3;
+        private Panel panel4;
+        private TextBox txbSearch;
+        private ListView listView1;
+        private Panel panel7;
+        private Panel panel6;
+        private Panel panel8;
+        private Panel panel5;
+        private Label label2;
+        private Button button1;
+        private Button btn_Thanhtoan;
+        private Button button2;
+        private NumericUpDown numericUpDown1;
+        private ListView lv_Menu;
+        private ImageList imageList1;
+        private Button btn_search;
+    }
+}
---
+++ PresentationLayer/chamcong.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace PresentationLayer
+{
+    partial class chamcong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            listView1 = new ListView();
+            txt_maca = new TextBox();
+            lb_maca = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(6, 7);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(916, 54);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(lb_maca);
+            panel2.Controls.Add(txt_maca);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(7, 67);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(432, 421);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(listView1);
+            panel3.Controls.Add(label2);
+            panel3.Location = new Point(445, 67);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(470, 421);
+            panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(92, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(246, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Thông tin ca làm";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(98, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(287, 41);
+            label2.TabIndex = 1;
+            label2.Text = "Thông tin nhân viên";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(7, 51);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(460, 312);
+            listView1.TabIndex = 2;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // txt_maca
+            // 
+            txt_maca.Location = new Point(117, 72);
+            txt_maca.Multiline = true;
+            txt_maca.Name = "txt_maca";
+            txt_maca.Size = new Size(271, 34);
+            txt_maca.TabIndex = 1;
+            // 
+            // lb_maca
+            // 
+            lb_maca.AutoSize = true;
+            lb_maca.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lb_maca.Location = new Point(27, 76);
+            lb_maca.Name = "lb_maca";
+            lb_maca.Size = new Size(63, 25);
+            lb_maca.TabIndex = 2;
+            lb_maca.Text = "Mã ca";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(117, 127);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(271, 34);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(117, 190);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(271, 34);
+            textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(20, 196);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Giờ đóng";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(25, 133);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Giờ mở";
+            // 
+            // chamcong
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(927, 500);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "chamcong";
+            Text = "chamcong";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox txt_maca;
+        private Label label1;
+        private Panel panel3;
+        private ListView listView1;
+        private Label label2;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label lb_maca;
+    }
+}
